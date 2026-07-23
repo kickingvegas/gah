8e5468ac4380c720d881bc45abd50c4bdf17cbd4
Charles Choi <kickingvegas@gmail.com>
2026-07-23 11:21:09 -0700

    Bump version to 0.1.2-rc.1-rc.3

--- lisp/gah.el
+++ lisp/gah.el
@@ -4,7 +4,7 @@
 
 ;; Author: Charles Choi <kickingvegas@gmail.com>
 ;; Keywords: tools
-;; Version: 0.1.2-rc.1-rc.2
+;; Version: 0.1.2-rc.1-rc.3
 ;; Package-Requires: ((emacs "30.1") (transient "0.9.0") (ox-gfm "1.0"))
 
 ;; This program is free software; you can redistribute it and/or modify
